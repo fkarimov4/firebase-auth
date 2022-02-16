7d777dad0f41b95c0eb230ab2107dec7c8a7dc8a
Farrukh Karimov <fkarimov4@gmail.com>
2022-02-16 16:06:05 -0500

    added sign out functionality and used localStorage to save data about the user

--- src/scenes/Login.js
+++ src/scenes/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import {
   getAuth,
@@ -9,31 +9,46 @@ import {
 } from "firebase/auth";
 import { app } from "../ConnectAuth";
 
-function Login({ setUser }) {
+function Login({ user, setUser }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
   const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  useEffect(() => {
+    const localUser = localStorage.getItem("displayName");
+
+    if (localUser) {
+        setUser(localUser)
+    }
+
+  }, []);
+
   const handleGoogleLogin = () => {
     signInWithPopup(auth, provider)
-    .then((result) => {
+      .then((result) => {
         setUser(result.user);
-        navigate("/");
+        localStorage.setItem("displayName", result.user.displayName);
+        localStorage.setItem("profilePhoto", result.user.photoURL)
+        navigate('/')
       })
+
+
       .catch(alert);
-  }
+  };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
       .then((result) => {
         setUser(result.user);
+
         navigate("/");
       })
       .catch(alert);
   };
+
   return (
     <>
       <h1>Login</h1>
@@ -61,13 +76,14 @@ function Login({ setUser }) {
         <br />
         <input type="submit" value="Login" />
       </form>
-      <button onClick={handleGoogleLogin}
+      <button
+        onClick={handleGoogleLogin}
         style={{
           backgroundColor: "black",
           color: "white",
           border: "none",
           padding: "5px 20px",
-          margin: '10px 0'
+          margin: "10px 0",
         }}
       >
         Sign in with Google
--- src/scenes/Welcome.js
+++ src/scenes/Welcome.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 function Welcome({ user, setUser }) {
-    console.log(user)
-    return (
-        <>
-            <h1>Welcome!</h1>
-            <h2>{user.displayName || user.email}</h2>
-            {user.photoURL && <img src={user.photoURL} alt={'Profile picture of logged-in user'} />}
-            <button onClick={() => setUser(false)}>Sign out</button>
-        </>
-    )
+  const auth = getAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+      signOut(auth)
+      .then(() => {
+        localStorage.clear();
+        navigate("/login");
+    })
+    .catch(console.error())
+  };
+
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <h2>{localStorage.getItem("displayName") || user.email}</h2>
+      {localStorage.getItem("profilePhoto") && (
+        <img
+          src={localStorage.getItem("profilePhoto")}
+          alt={"Profile picture of logged-in user"}
+        />
+      )}
+      <button onClick={handleLogout}>Sign out</button>
+    </>
+  );
 }
 
-export default Welcome
+export default Welcome;
